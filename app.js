69dc4322fa6c5def57a2fdf8d6673e7b6081549d
Aleksey0808 <103374981+Aleksey0808@users.noreply.github.com>
2023-05-16 19:26:52 +0300

    init hm03

--- app.js
+++ app.js
@@ -14,9 +14,9 @@ app.use(express.json())
 
 app.use('/api/contacts', contactsRouter)
 
-app.listen(3000, () => {
-  console.log('Example app listening on port 3000!')
-})
+// app.listen(3000, () => {
+//   console.log('Example app listening on port 3000!')
+// })
 
 app.use((req, res) => {
   res.status(404).json({ message: 'Not found' })
--- server.js
+++ server.js
@@ -1,5 +1,16 @@
+const mongoose = require('mongoose')
 const app = require('./app')
-
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+// WI09JrGVP0yzSGSO
+const DB_HOST =
+  'mongodb+srv://Aleksey:WI09JrGVP0yzSGSO@cluster0.insm7jq.mongodb.net/db-contacts?retryWrites=true&w=majority'
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(3000, () => {
+      console.log('Server running. Use our API on port: 3000')
+    })
+  })
+  .catch((error) => {
+    console.log(error.message)
+    process.exit(1)
+  })
